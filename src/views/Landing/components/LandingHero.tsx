1eef6e34976f78cf1b1b0ba117425a6a5839ef0f
Hossein <hossein.salari002@tasu.ventures>
2025-02-24 01:04:58 +0330

    feat: enhance LandingHero with DecryptedText and RotatingText components

--- src/views/Landing/components/LandingHero.tsx
+++ src/views/Landing/components/LandingHero.tsx
@@ -1,40 +1,41 @@
 import ShinyText from "@/reactBits/ShinyText/ShinyText";
-import TextPressure from "@/reactBits/TextPressure/TextPressure";
-import React, { useEffect, useState } from "react";
-import { ArrowDown } from "../icons/Icons";
+import DecryptedText from "@/reactBits/DecryptedText/DecryptedText";
+import RotatingText from "@/reactBits/RotatingText/RotatingText";
 
 function LandingHero() {
-  //   const [hydrated, setHydrated] = useState(false);
-
-  //   useEffect(() => {
-  //     // this forces a rerender
-  //     setHydrated(true);
-  //   }, []);
-
-  //   if (!hydrated) {
-  //     // this returns null on first render, so the client and server match
-  //     return null;
-  //   }
-
   return (
     <div className="text-start mt-[80px] flex flex-col justify-end px-16">
-      <h1 className="text-[#121619] text-2xl font-normal">
-        Hey, I’m <span className="font-bold">Hossein</span>
-      </h1>
-      {/* <TextPressure
-        stroke={false}
-        italic={false}
-        flex={false}
-        // weight={false}
-        alpha={false}
-        strokeWidth={26}
+      <DecryptedText
         text="Hey, I’m Hossein"
-        textColor="#000"
-      /> */}
-      <h1 className="text-9xl font-bold text-[#121619] mt-2 leading-tight">
-        Building <span className="font-light">Seamless</span>
-        <br /> Digital Experiences
-      </h1>
+        speed={60}
+        maxIterations={20}
+        characters="ABCD1234!?"
+        animateOn="hover"
+        revealDirection="start"
+        sequential={true}
+        parentClassName="text-2xl w-fit"
+      />
+
+      <div className="text-9xl font-bold text-[#121619] mt-2 leading-tight">
+        <h1 className="flex items-center justify-start gap-8">
+          Building
+          <span>
+            {" "}
+            <RotatingText
+              texts={["Seamless", "Effortless", "Smooth", "Streamlined"]}
+              staggerFrom={"random"}
+              initial={{ y: "100%", opacity: "1" }}
+              animate={{ y: 0, opacity: "1" }}
+              exit={{ y: "-120%", opacity: "0" }}
+              staggerDuration={0.025}
+              splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1 font-light"
+              transition={{ type: "spring", damping: 40, stiffness: 400 }}
+              rotationInterval={4000}
+            />
+          </span>
+        </h1>
+        <h1>Digital Experiences</h1>
+      </div>
 
       <div className="flex items-center justify-between w-full mt-10">
         <button className="bg-[#0F62FE] rounded-full flex items-center justify-center gap-2 text-xl font-normal text-white">
